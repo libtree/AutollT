fdf4120555cc4c56f417d1382e56d2aa22d5aea0
libtree <100961732+libtree@users.noreply.github.com>
2021-02-21 17:11:48 +0000

    I18n: Update translation el (100%).

--- big_float.js
+++ big_float.js
@@ -43,3 +43,100 @@ function is_negative(big) {
 function is_positive(big) {
     return big_integer.is_positive(big.coefficient);
 }
+
+function is_zero(big) {
+    return big_integer.is_zero(big.coefficient);
+}
+
+const zero = Object.create(null);
+zero.coefficient = big_integer.zero;
+zero.exponent = 0;
+Object.freeze(zero);
+
+function make_big_float(coefficient, exponent) {
+    if (big_integer.is_zero(coefficient)) {
+        return zero;
+    }
+    const new_big_float = Object.create(null);
+    new_big_float.coefficient = coefficient;
+    new_big_float.exponent = exponent;
+    return Object.freeze(new_big_float);
+}
+
+const big_integer_ten_million = big_integer.make(10000000);
+
+function number(a) {
+    const result = (
+        is_big_float(a)
+        ? (
+            a.exponent === 0
+            ? big_integer.number(a.coefficient)
+            : big_integer.number(a.coefficient) * (10 ** a.exponent)
+        )
+        : (
+            typeof a === "number"
+            ? a
+            : (
+                big_integer.is_big_integer(a)
+                ? big_integer.number(a)
+                : Number(a)
+            )
+        )
+    );
+    return (
+        Number.isFinite(result)
+        ? result
+        : undefined
+    );
+}
+
+function neg(a) {
+    return make_big_float(big_integer.neg(a.coefficient), a.exponent);
+}
+
+function abs(a) {
+    return (
+        is_negative(a)
+        ? neg(a)
+        : a
+    );
+}
+
+function conform_op(op) {
+    return function (a, b) {
+        const differential = a.exponent - b.exponent;
+        return (
+            differential === 0
+            ? make_big_float(op(a.coefficient, b.coefficient), a.exponent)
+            : (
+                differential > 0
+                ? make_big_float(
+                    op(
+                        big_integer.mul(
+                            a.coefficient,
+                            big_integer.power(big_integer.ten, differential)
+                        ),
+                        b.coefficient
+                    ),
+                    b.exponent
+                )
+                : make_big_float(
+                    op(
+                        a.coefficient,
+                        big_integer.mul(
+                            b.coefficient,
+                            big_integer.power(big_integer.ten, -differential)
+                        )
+                    ),
+                    a.exponent
+                )
+            )
+        );
+    };
+}
+
+const add = conform_op(big_integer.add);
+const sub = conform_op(big_integer.sub);
+
+function eq(comparahend, comparator) {
+    return comparahend === comparator || is_zero(sub(comparahend, comparator));

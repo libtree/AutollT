ef0c2d9bac13a368614802a8a3e481ff7f5f8e56
libtree <100961732+libtree@users.noreply.github.com>
2021-03-07 06:37:53 +0000

    add register google

--- big_integer.js
+++ big_integer.js
@@ -5,4 +5,7 @@
 // You can access the big integer object in your module by importing it.
 //      import big_integer from "./big_integer.js";
 
-/*
+/*jslint bitwise, for */
+
+/*property
+    abs, abs_lt, add, and, asIntN, asUintN, clz32, crea

2cd9b6c9e34ca8319bee75e0ef648d11c1e09c60
libtree <100961732+libtree@users.noreply.github.com>
2021-07-04 11:21:32 +0000

    Update git.html

--- big_integer.js
+++ big_integer.js
@@ -209,4 +209,9 @@ function make(value, radix_2_37 = 10) {
     radix_2_37 = number(radix_2_37);
     if (
         !Number.isInteger(radix_2_37)
-      
+        || radix_2_37 < 2
+        || radix_2_37 > 37
+    ) {
+        return undefined;
+    }
+    if (r

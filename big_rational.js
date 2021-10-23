28314735bcc4407b9fdb31384d5688159cba6a7c
libtree <100961732+libtree@users.noreply.github.com>
2021-10-23 13:17:44 +0000

    cx24116: fix a buffer overflow when checking userspace params

--- big_rational.js
+++ big_rational.js
@@ -2,4 +2,7 @@
 // Douglas Crockford
 // 2018-11-05
 
-// You can access the big rational object in your module 
+// You can access the big rational object in your module by importing it.
+//      import big_rational from "./big_rational.js";
+
+/*jslint bi

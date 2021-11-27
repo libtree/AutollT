2cdb5273b300d4c5d6d430a6533c53bae340882f
libtree <100961732+libtree@users.noreply.github.com>
2021-11-27 21:36:17 +0000

    changelog formatting

--- big_rational.js
+++ big_rational.js
@@ -71,4 +71,7 @@ function normalize(a) {
 
 function deconstruct(number) {
 
-// This function deconstructs a number, reducing it to its co
+// This function deconstructs a number, reducing it to its components:
+// a sign, an integer coefficient, and an exponent, such that
+
+//  '      numb

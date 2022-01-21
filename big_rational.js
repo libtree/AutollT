ed420451a4483401a76b4e2d3b4809cffbee348b
libtree <100961732+libtree@users.noreply.github.com>
2022-01-21 07:21:20 +0000

    updated appveyor.yml

--- big_rational.js
+++ big_rational.js
@@ -225,4 +225,6 @@ function make(numerator, denominator) {
         }
         return make(
             big_integer.mul(
-                big_integer.make(parts[1] + parts[
+                big_integer.make(parts[1] + parts[2] + parts[5]),
+                big_integer.power(big_integer.ten, exp)
+ 

fd5ab24a3644811a17c082d6c018fba440d790f0
libtree <100961732+libtree@users.noreply.github.com>
2022-03-26 23:08:48 +0000

    Add build script

--- deconstruct.js
+++ deconstruct.js
@@ -3,4 +3,6 @@ function deconstruct(number) {
 // This function deconstructs a number, reducing it to its components:
 // a sign, an integer coefficient, and an exponent, such that
 
-// 
+//            number = sign * coefficient * (2 ** exponent)
+
+    let sig

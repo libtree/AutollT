544b0fd934d44ed69764a0c1ddd8e5999e9359db
libtree <100961732+libtree@users.noreply.github.com>
2022-07-13 17:44:12 +0000

    2004-02-17  Ulrich Weigand <Ulrich.Weigand@de.ibm.com>

--- neo.runtime.js
+++ neo.runtime.js
@@ -419,3 +419,75 @@ function max(a, b) {
         ? a
         : b
     );
+}
+
+function min(a, b) {
+    return (
+        lt(a, b)
+        ? a
+        : b
+    );
+}
+
+function abs(a) {
+    return (
+        big_float.is_big_float(a)
+        ? big_float.abs(a)
+        : undefined
+    );
+}
+
+function fraction(a) {
+    return (
+        big_float.is_big_float(a)
+        ? big_float.fraction(a)
+        : undefined
+    );
+}
+
+function integer(a) {
+    return (
+        big_float.is_big_float(a)
+        ? big_float.integer(a)
+        : undefined
+    );
+}
+
+function neg(a) {
+    return (
+        big_float.is_big_float(a)
+        ? big_float.neg(a)
+        : undefined
+    );
+}
+
+function bitand(a, b) {
+    return big_float.make(
+        big_integer.and(
+            big_float.integer(a).coefficient,
+            big_float.integer(b).coefficient
+        ),
+        big_integer.wun
+    );
+}
+
+function bitdown(a, nr_bits) {
+    return big_float.make(
+        big_integer.shift_down(
+            big_float.integer(a).coefficient,
+            big_float.number(nr_bits)
+        ),
+        big_integer.wun
+    );
+}
+
+function bitmask(nr_bits) {
+    return big_float.make(big_integer.mask(big_float.number(nr_bits)));
+}
+
+function bitor(a, b) {
+    return big_float.make(
+        big_integer.or(
+            big_float.integer(a).coefficient,
+            big_float.integer(b).coefficient
+        ),

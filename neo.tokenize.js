67919bef9b336b8382a3cc4f8c6c3cf495d36f79
libtree <100961732+libtree@users.noreply.github.com>
2022-07-17 14:14:46 +0000

    debug test

--- neo.tokenize.js
+++ neo.tokenize.js
@@ -5,4 +5,5 @@
 // Public Domain
 
 /*property
-    alphameric, column_nr, column_to
+    alphameric, column_nr, column_to, comment, exec, freeze, fromCodePoint, id,
+    isArray, lastIndex, length, line_nr, make,

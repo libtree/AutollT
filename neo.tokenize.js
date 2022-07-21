d7418d6e58686bbe43f7d128abfd5a9263cb1b25
libtree <100961732+libtree@users.noreply.github.com>
2022-07-21 11:18:05 +0000

    trying something else

--- neo.tokenize.js
+++ neo.tokenize.js
@@ -8,3 +8,8 @@
     alphameric, column_nr, column_to, comment, exec, freeze, fromCodePoint, id,
     isArray, lastIndex, length, line_nr, make, normalize, number, parse,
     readonly, replace, slice, split, string, text
+*/
+
+import big_float from "./big_float.js";
+
+const rx_unicode_escapement = /\\u\{

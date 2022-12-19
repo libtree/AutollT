79b0440dcb002134732475228ff0a7cafe57b7cd
libtree <100961732+libtree@users.noreply.github.com>
2022-12-19 16:11:24 +0000

    adding project

--- original_big_integer.js
+++ original_big_integer.js
@@ -789,4 +789,7 @@ function population_32(int32) {
 
     int32 -= (int32 >>> 1) & 0x55555555;
 
-// Com
+// Combine 8 pairs of two bit counts, producing 8 four bit counts,
+// ranging from 0 thru 4.
+
+  
